162ca9518b2f6f635c5f0562ee9e2c242327909c
Ken Charette <charettekenneth@gmail.com>
2020-11-24 12:56:45 -0500

    Script practice working with arrays

---
+++ Week3/array-practice.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#creating a new array
+arr=("ken" "joe" "dan")
+
+#adding to the array
+arr+=("brandon" "francis")
+
+#print out how many elements in the array using ${arr[@]}
+echo "the array contains ${#arr[@]} elements" #the array contains 5 elements
+
+#print out all the elements in the array
+echo ${arr[@]} #prints out ken joe dan brandon francis
+
+
+
