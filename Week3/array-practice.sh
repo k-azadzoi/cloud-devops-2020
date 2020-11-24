d3c908606ef8379b1bfe5a2d133f3b344785a5ed
Ken Charette <charettekenneth@gmail.com>
2020-11-24 17:33:18 -0500

    Script for comparing numbers with ifelse

--- Week3/array-practice.sh
+++ Week3/array-practice.sh
@@ -8,9 +8,6 @@ arr+=("brandon" "francis")
 
 #print out how many elements in the array using ${arr[@]}
 echo "the array contains ${#arr[@]} elements" #the array contains 5 elements
-
+c
 #print out all the elements in the array
 echo ${arr[@]} #prints out ken joe dan brandon francis
-
-
-
