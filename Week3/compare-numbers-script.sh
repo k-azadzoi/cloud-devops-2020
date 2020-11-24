798acd10495efa1b6ef181414b2fc50d3cf847b8
Ken Charette <charettekenneth@gmail.com>
2020-11-24 17:39:08 -0500

    Comments added to recent script and cleaned up code

--- Week3/compare-numbers-script.sh
+++ Week3/compare-numbers-script.sh
@@ -1,16 +1,16 @@
-x=15
-y=15
+#! /bin/bash
+#A simple script that will two numbers and compare them.
 
-echo $x
-echo $y
+read x 
+read y
 
-if [ "$x" -gt "$y" ]
+if [ "$x" -gt "$y" ] #checks if x is greater than y then output a message 
     then
     echo "X is greater than Y"
-elif [ "$x" -lt "$y" ]
+elif [ "$x" -lt "$y" ] #checks if x is less than y then output a message
     then    
     echo "X is less than Y"
-elif [ "$x" -eq "$y" ]
+elif [ "$x" -eq "$y" ] #checks if x equal to y then output a message
     then
     echo "X is equal to Y"
 fi
