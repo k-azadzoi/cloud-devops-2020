633d94586ac57494fdaafdcbebfe0b25025b8d55
Ken Charette <charettekenneth@gmail.com>
2020-11-30 14:17:03 -0500

    New repo for learning python

---
+++ Week4/intro-to-python/naming-conventions.py
@@ -0,0 +1,17 @@
+# Different naming conventions used in Python
+
+def thisIsCamelCasing():
+    x = "Hello World"
+    print(x)
+
+
+def ThisIsPascalCasing():
+    return None
+
+
+def this_is_snake_case():
+    return None
+
+
+# When run it will print out Hello World
+thisIsCamelCasing()
