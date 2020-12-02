56e8e5818f09497a4ee550dd4948be7f80bdd961
Ken Charette <charettekenneth@gmail.com>
2020-12-02 11:37:16 -0500

    More practice with loops in python

--- Week4/intro-to-python/working_with_loops.py
+++ Week4/intro-to-python/working_with_loops.py
@@ -1,5 +1,24 @@
 # Learning about loops in Python
 
+# For loops
+
+# Printing out each item in an array called shopping_list
+shopping_list = ["apples", "milk", "bread"]
+
+for item in shopping_list:
+    print(item)
+
+# Loop through an array of numbers
+numbers = [20, 10, 7]
+total = 0
+
+for num in numbers:
+    total = total + num
+
+print(total)
+
+
+# Looping through a range of numbers starting at 0 and ending at 4
 for i in range(0, 5):  # this will print 0 1 2 3 4 on separate lines
     print(i)
 
@@ -11,4 +30,6 @@ i = 0
 while i < 5:  # printing 0 1 2 3 4 using a while loop
     print(i)
     i += 1
+
+
 
