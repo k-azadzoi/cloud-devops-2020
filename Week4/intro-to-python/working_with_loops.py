f0a136d23fc6efa1093acd77090100e53d009830
Ken Charette <charettekenneth@gmail.com>
2020-12-01 20:04:26 -0500

    Practicing loops in python

---
+++ Week4/intro-to-python/working_with_loops.py
@@ -0,0 +1,14 @@
+# Learning about loops in Python
+
+for i in range(0, 5):  # this will print 0 1 2 3 4 on separate lines
+    print(i)
+
+
+for i in range(10, 0, -1):  # will countdown starting at 10 and ending at 1
+    print(i)
+
+i = 0
+while i < 5:  # printing 0 1 2 3 4 using a while loop
+    print(i)
+    i += 1
+
