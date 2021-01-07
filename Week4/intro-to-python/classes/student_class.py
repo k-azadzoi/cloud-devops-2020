9b2fc35551b044e7d146c3720a634a6b2a0ba95e
Ken Charette <charettekenneth@gmail.com>
2021-01-06 20:57:26 -0500

    More practice. Added movie_class file

--- Week4/intro-to-python/classes/student_class.py
+++ Week4/intro-to-python/classes/student_class.py
@@ -1,6 +1,11 @@
 #   Practice to learn how to make classes in python
 
 class Student:
+
+    #   class attribute / defining a constant
+    school_name = "Peppermint High"
+
+    #   initializer / instance attributes
     def __init__(self, student_id, name, address):
         #   init is a special method called the initializer
         #   self is always the first parameter because it gives access to the object's properties
@@ -22,9 +27,14 @@ student_one.email = "manbearpig@supercereal.com"
 print("The student's email is " + student_one.email)
 #   This will output "The student's email is manbearpig@supercereal.com"
 
+print("The school name is " + Student.school_name)
 
 
-
-
-
+print(hasattr(student_one, 'name'))
+#   hasattr will check if the attribute exists
+print(getattr(student_one, 'email'))
+#   getattr accesses the attribute of the object
+setattr(student_one, 'age', '17')
+#   setattr creates a new instance of attribute 'age' and sets the value
+print(student_one.age)
 
