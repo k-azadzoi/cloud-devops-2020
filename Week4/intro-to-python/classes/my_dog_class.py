9774b30adf6d08e29bf8c76cd7d531222ee87ff2
Ken Charette <charettekenneth@gmail.com>
2021-01-14 00:09:05 -0500

    Explanations added to the Dog class

--- Week4/intro-to-python/classes/my_dog_class.py
+++ Week4/intro-to-python/classes/my_dog_class.py
@@ -1,5 +1,7 @@
-class Dog:
+# A simple example of a class in python using instance methods.
 
+class Dog:
+    # using instance attributes to define the dog class
     def __init__(self, breed, name, age, color, is_asleep=False):
         self.breed = breed
         self.name = name
@@ -7,12 +9,16 @@ class Dog:
         self.color = color
         self.is_asleep = is_asleep
 
+    # creating instance methods that use self to reference the instance attributes
     def walk(self):
         return self.name + ' is walking!'
 
     def eat(self):
         return self.name + ' is eating!'
 
+    # since I am changing the is_asleep attribute to True.
+    # In the return statement I convert the is_alseep attribute to a string
+    # That way it can be outputted to the console in the print statement
     def sleep(self):
         self.is_asleep = True
         return 'It is ' + str(self.is_asleep) + '. ' + self.name + ' is sleeping!'
