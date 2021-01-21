5ef3d27c9f1c451807eca16d49573402f76a2038
Ken Charette <charettekenneth@gmail.com>
2021-01-20 22:14:55 -0500

    More examples of inheritance

--- Week4/intro-to-python/inheritance/intro_to_inheritance.py
+++ Week4/intro-to-python/inheritance/intro_to_inheritance.py
@@ -26,6 +26,11 @@ class Student(Person):
         self.graduated = False
         super().__init__(first_name, last_name)
 
+# In the Student subclass, we override the __init__ method so that it can be used to initialize
+# the class' attributes. At the same time we want the parents' class attributes to be initialized as well.
+# That is the purpose of the super function.
 
 student_one = Student("Max", "Power", 'Intro to Python', True)
 print("{} {} is a student taking the {} course. ".format(student_one.first_name, student_one.last_name, student_one.course))
+
+
