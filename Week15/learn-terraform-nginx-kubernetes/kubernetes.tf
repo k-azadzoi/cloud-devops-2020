bd90fd966b244d21f000fc295b9b3801a191eaf9
Ken Charette <charettekenneth@gmail.com>
2021-02-18 13:28:56 -0500

    Added code to kuberentes.tf file

---
+++ Week15/learn-terraform-nginx-kubernetes/kubernetes.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+provider "kubernetes" {
+  host = var.host
+
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
+}
